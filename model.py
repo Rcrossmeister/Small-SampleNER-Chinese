b362817ee5820dc601ecc728f11b0575bbb847a1
maidangdang1 <mmt869601840@gmail.com>
2023-05-12 17:08:04 +0800

    feat: add BERT_BiLSTM_CRF model

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
+import torch
 from torch.nn import LayerNorm
 import torch.nn as nn
 from crf import CRF
+from transformers import BertModel
+from typing import Optional
+import logging
+from torchcrf import CRF as torchCRF
 
 class SpatialDropout(nn.Dropout2d):
     def __init__(self, p=0.6):
@@ -43,3 +48,73 @@ class NERModel(nn.Module):
             return features, self.crf.calculate_loss(features, tag_list=input_tags, lengths=input_lens)
         else:
             return features
+
+class BERT_BiLSTM_CRF:
+    def __init__(self, num_classes: int, bert_model_name: str, device: str,
+                 hidden_dim: Optional[int] = 256, num_layers: Optional[int] = 2):
+        super().__init__()
+        self.device = device
+        self.bert = BertModel.from_pretrained(bert_model_name).to(device)
+        self.lstm = nn.LSTM(input_size=self.bert.config.hidden_size, hidden_size=hidden_dim, num_layers=num_layers,
+                            batch_first=True, bidirectional=True).to(device)
+        self.dropout = nn.Dropout(p=0.1)
+        self.hidden2label = nn.Linear(hidden_dim * 2, num_classes).to(device)
+        self.crf = torchCRF(num_classes, batch_first=True).to(device)
+
+    def forward(self, x):
+        outputs = self.bert(**x)
+        sequence_output = outputs.last_hidden_state
+        sequence_output, _ = self.lstm(sequence_output)
+        sequence_output = self.dropout(sequence_output)
+        emissions = self.hidden2label(sequence_output)
+        return emissions
+
+    def loss(self, x, y_pred, y) -> torch.Tensor:
+        mask = x['attention_mask'].bool()
+
+        logging.debug(mask)
+
+        log_likelihood = self.crf(y_pred, y, mask=mask)
+        loss = -log_likelihood
+
+        logging.debug(loss)
+        logging.debug(f"loss.shape: {loss.shape}")
+
+        loss_item = loss.item()
+        loss.backward()
+        return loss_item
+
+    def decode(self, emissions: torch.Tensor, mask: torch.ByteTensor) -> torch.Tensor:
+        mask = mask.bool()
+        return self.crf.decode(emissions, mask=mask)
+
+    def predict(self, x):
+        y_pred = self.forward(x)
+        mask = x['attention_mask'].bool()
+        logging.debug(y_pred.shape)
+        y_pred = self.crf.decode(y_pred, mask=mask)
+        logging.debug(y_pred)
+        logging.debug(len(y_pred))
+        # assert False
+        return y_pred
+
+    def switch_mode(self, mode):
+        if mode == 'train':
+            self.train()
+        elif mode == 'eval':
+            self.eval()
+        else:
+            raise ValueError(f"Invalid mode {mode}")
+
+    def to(self, x, y):
+        x["input_ids"] = x["input_ids"].to(self.device)
+        x["attention_mask"] = x["attention_mask"].to(self.device)
+        return x, y.to(self.device)
+
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+
+    def train_model(self, x, y):
+        y_pred = self.forward(x)
+        loss_item = self.loss(x, y_pred, y)
+        return loss_item
