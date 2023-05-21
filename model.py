fe93faa8b6890e348d45022c6a8756489eeebd5d
i_Corner <80880063+i-Corner@users.noreply.github.com>
2023-05-21 10:36:14 +0800

    Update model.py for Loss_net to active_learn

--- model.py
+++ model.py
@@ -155,5 +155,44 @@ class BERT_BiLSTM_CRF(nn.Module):
         loss = -self.crf(y_pred, y_true, mask=mask)
         return y_pred, loss
 
+class LossNet(nn.Module):
+    def __init__(self, feature_sizes=[768, 768, 768, 12], num_channels=[512, 512, 512, 512], interm_dim=128):
+        """
+        参数需根据backbone模型每层输出特征以及embedding形式对应修改
+        """
+        super(LossNet, self).__init__()
+        
+        self.GAP1 = nn.AvgPool1d(feature_sizes[0])
+        self.GAP2 = nn.AvgPool1d(feature_sizes[1])
+        self.GAP3 = nn.AvgPool1d(feature_sizes[2])
+        self.GAP4 = nn.AvgPool1d(feature_sizes[3])
 
+        self.FC1 = nn.Linear(num_channels[0], interm_dim)
+        self.FC2 = nn.Linear(num_channels[1], interm_dim)
+        self.FC3 = nn.Linear(num_channels[2], interm_dim)
+        self.FC4 = nn.Linear(num_channels[3], interm_dim)
+
+        self.linear = nn.Linear(4 * interm_dim, 1)
+    
+    def forward(self, features):
+
+        #raise BaseException("OK")
+        out1 = self.GAP1(features[0])
+        out1 = out1.view(out1.size(0), -1)
+        out1 = F.relu(self.FC1(out1))
+        
+        out2 = self.GAP2(features[1])
+        out2 = out2.view(out2.size(0), -1)
+        out2 = F.relu(self.FC2(out2))
+
+        out3 = self.GAP3(features[2])
+        out3 = out3.view(out3.size(0), -1)
+        out3 = F.relu(self.FC3(out3))
+
+        out4 = self.GAP4(features[3])
+        out4 = out4.view(out4.size(0), -1)
+        out4 = F.relu(self.FC4(out4))
+
+        out = self.linear(torch.cat((out1, out2, out3, out4), 1))
+        return out
 
